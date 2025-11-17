806a6ea1ce849369a9b03939921b83941bf0f17d
Rian Souza <rian.souza.ti@gmail.com>
2025-11-17 17:49:30 -0300

    feat: novo sac criado

---
+++ sac.php
@@ -0,0 +1,25 @@
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
+
+
+<main>
+<h2>SAC</h2>
+<form>
+<label>Nome:</label><br>
+<input type="text"><br><br>
+
+
+<label>Email:</label><br>
+<input type="text"><br><br>
+
+
+<label>Mensagem:</label><br>
+<textarea></textarea><br><br>
+
+
+<button>Enviar</button>
+</form>
+</main>
+
+
+<?php include 'footer.php'; ?>
