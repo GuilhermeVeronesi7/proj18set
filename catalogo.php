d25632e777da055b92f244c93bd40bfd77a6f8e5
Rian Souza <rian.souza.ti@gmail.com>
2025-11-17 17:46:17 -0300

    feat: novo arquivo catalogo criado

---
+++ catalogo.php
@@ -0,0 +1,20 @@
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
+
+
+<main>
+<h2>Catálogo de Livros</h2>
+
+
+<button onclick="abrirFormulario()">Cadastrar Novo Livro</button>
+
+
+<div id="lista"></div>
+</main>
+
+
+<script src="js/listar.js"></script>
+<script src="js/form.js"></script>
+
+
+<?php include 'footer.php'; ?>
