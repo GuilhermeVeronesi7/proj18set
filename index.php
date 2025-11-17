b7f3666e5223f7d3f37e73f8556afaaa57b27461
Rian Souza <rian.souza.ti@gmail.com>
2025-11-17 17:49:01 -0300

    feat: novo index criado

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+<?php include 'nav.php'; ?>
+
+
+<main>
+<h2>Bem-vindo ao CRUD de Livros</h2>
+<p>Projeto didático integrado com API em PHP.</p>
+</main>
+
+
+<?php include 'footer.php'; ?>
